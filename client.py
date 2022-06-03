740ee99e8faee8dd558bc1d51f6498f00f00f946
Diego Joel Condori Quispe <93290842+diegojoel301@users.noreply.github.com>
2022-06-03 21:37:08 +0200

    modificado

--- client.py
+++ client.py
@@ -1,17 +1,66 @@
 import socket
+def automata_finito(caracter):
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+                'l', 'm', 'n', 'ñ', 'o','p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z', ' ']
 
-#mi_socket = socket.socket()
+        TablaT = [['ini', 'a', '.-'], ['ini', 'b', '-..'], ['ini', 'c', '-.-.'], 
+                ['ini', 'd', '-..'], ['ini', 'e', '.'], ['ini', 'f', '..-.'], ['ini', 'g', '--.'],
+                ['ini', 'h', '....'], ['ini', 'i', '..'], ['ini', 'j', '.---'], ['ini', 'k', '-.-'],
+                ['ini', 'l', '.-..'], ['ini', 'm', '--'], ['ini', 'n', '-.'], ['ini', 'ñ', '--.--'],
+                ['ini', 'o', '---'], ['ini', 'p', '.--.'], ['ini', 'q', '--.-'], ['ini', 'r', '.-.'],
+                ['ini', 's', '...'], ['ini', 't', '-'], ['ini', 'u', '..-'], ['ini', 'v', '...-'], 
+                ['ini', 'w', '.--'], ['ini', 'x', '-..-'], ['ini', 'y', '-.--'], ['ini', 'z', '--..'],
+                ['ini', ' ', '/']]
 
-#mi_socket.connect( ('localhost', 8000) )
+        bandera = True
 
-user = input("[+] Introduce tu username: ")
+        TablaC = [] # Tabla para almacenar los estados que se mueven
+
+        EI = 'ini' # Estado inicial q0
+
+        EF = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', 
+                '..', '.---', '-.-', '.-..', '--', '-.', '--.--',  '---', '.--.', '--.-', 
+                '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', 
+                '/', 'ini'] # Estado Final qf
+
+        EA = EI # Estado Actual
+
+        # Verificar que el caracter pertenezca al alphabet
+        if caracter in alphabet:
+                # Buscar en la tabla el caracter
+                for f in TablaT:
+                        # Recorrer las transiciones de la tabla
+                        # Indicar el estado actual y el estado final
+                        if caracter == f[1] and EA == f[0]:
+                                # Agregar elementos a la tabla comparativa
+                                TablaC.append([EA, caracter, f[2]])
+                                # Actualizar el estado
+                                EA = f[2]
+                                break
+        else:
+                print("Cadena no pertenece al alfabeto: " + caracter)
+                bandera = False
+
+        # Comparar si el estado actual es igual al estado final
+
+        if EA in EF and bandera == True:
+                return EA
+        return "-1"
+
+def codifica_cadena(cadena):
+        salida = str()
+        for caracter in cadena:
+                salida += automata_finito(caracter) + " "
+        return salida[0:len(salida) - 1]
 
 while True:
         mi_socket = socket.socket()
 
-        mi_socket.connect( ('localhost', 8000) )
+        mi_socket.connect( ('192.168.146.128', 8000) )
 
-        mi_socket.send((f"<{user}>" + input(str(">"))).encode())
+        #print(mensaje)
+
+        mi_socket.send(codifica_cadena(input(str(">"))).encode())
 
         respuesta = mi_socket.recv(1024)
 
